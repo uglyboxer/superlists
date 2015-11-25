e0cf2e828991f04c4050170c13f9c4b6cc2be0e8
Cole Howard <uglyboxer@gmail.com>
2015-11-24 16:24:23 -0800

    Need to build List object

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -40,16 +40,25 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -57,8 +66,10 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
-
+        self.assertEqual(first_saved_item.list, list_)
+        
 
 class NewListTest(TestCase):
 
